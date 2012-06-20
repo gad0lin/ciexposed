b30ef4b326eaac8dade336e9db775c31e3187e53
Krzysztof Barczynski <gadolin@gmail.com>
2012-06-20 23:55:38 +0200

    init maven project

---
+++ src/main/java/org/gadolin/exposed/App.java
@@ -0,0 +1,13 @@
+package org.gadolin.exposed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
